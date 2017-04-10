3378b997b9a462a30ef62824591eb45f23a9fcd6
Yvonne Liu <liuyvonne.ca@gmail.com>
2017-04-10 12:50:54 +0100

    make server run on https
    relates to #3

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const hapi = require('hapi');
 const inert = require('inert');
 const routes = require('./routes');
+const fs = require('fs');
+const path = require ('path');
 
 const server = new hapi.Server();
 
 server.connection({
   port: process.env.PORT || 3000,
+  tls: {
+       key: fs.readFileSync(path.join(__dirname, '../keys/key.pem'), 'utf8'),
+       cert: fs.readFileSync(path.join(__dirname, '../keys/cert.pem'), 'utf8')
+   }
 });
 
 server.register(inert, (err) => {
@@ -19,4 +25,4 @@ server.start((err) => {
   console.log(`Server is running at ${server.info.uri}`);
 });
 
-module.exports = server; 
+module.exports = server;
